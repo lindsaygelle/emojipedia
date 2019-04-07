1df8a216714aa98650d3e19a95f0e77ff76d3505
gellel <lindsay.gelle@gmail.com>
2019-04-07 14:04:11 +1000

    stash

---
+++ emojipedia-get/emojipedia-get.go
@@ -0,0 +1,21 @@
+package get
+
+const NAME string = "GET"
+
+var programs = []interface{}{about, category, description, subcategory, update}
+
+func Main(args []string) {
+
+}
+
+func Get(args ...string) {}
+
+func about(emoji string) {}
+
+func category(emoji string) {}
+
+func description(emoji string) {}
+
+func subcategory(emoji string) {}
+
+func update(emoji string) {}
