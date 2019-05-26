02af2a94f64dd7eba7e33609bfbcde69be58835b
Lindsay Gelle <lindsay.gelle@gmail.com>
2019-05-26 18:07:58 +1000

    Update arguments.go

--- arguments/arguments.go
+++ arguments/arguments.go
@@ -6,10 +6,13 @@ import (
 	"github.com/gellel/emojipedia/slice"
 )
 
+// New instantiates a new Arguments pointer.
 func New() *Arguments {
 	return &Arguments{slice: &slice.Slice{}}
 }
 
+// NewArguments builds an argument iterator from a collection of strings.
+// Assumes that os.Args[1:] is to be passed into the constructor function.
 func NewArguments(args []string) *Arguments {
 	arguments := New()
 	for _, arg := range args {
@@ -18,10 +21,12 @@ func NewArguments(args []string) *Arguments {
 	return arguments
 }
 
+// Arguments provides an iterator to move through the arguments fetched in from os.Args
 type Arguments struct {
 	slice *slice.Slice
 }
 
+// Get safely accesses the argument at the iteration index.
 func (pointer *Arguments) Get(i int) (argument string) {
 	if property, ok := pointer.slice.Get(i); ok {
 		argument = property.(string)
@@ -29,6 +34,7 @@ func (pointer *Arguments) Get(i int) (argument string) {
 	return argument
 }
 
+// Next unshifts the first element of the Arguments struct and returns the modified struct.
 func (pointer *Arguments) Next() *Arguments {
 	pointer.slice.Splice(1, pointer.slice.Len())
 	return pointer
