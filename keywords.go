1a4e6c17a5cf0489be998b29343fd1700cfb9f20
gellel <lindsay.gelle@gmail.com>
2019-06-06 18:01:23 +1000

    keyword iterator

--- keywords.go
+++ keywords.go
@@ -8,6 +8,19 @@ import (
 	"github.com/gellel/emojipedia/keywords"
 )
 
+func keywordsGet(arguments *arguments.Arguments) {
+	var (
+		keywords = keywords.Get()
+	)
+	fmt.Fprintln(writer, "N\t|Name\t|Emoji")
+	arguments.Each(func(i int, argument string) {
+		if slice, ok := keywords.Get(argument); ok {
+			fmt.Fprintln(writer, fmt.Sprintf("%v\t|%v\t|%vi", i, argument, slice.Join(" ")))
+		}
+	})
+	writer.Flush()
+}
+
 func keywordsKeys(arguments *arguments.Arguments) {
 	var (
 		keywords = keywords.Get()
@@ -19,11 +32,26 @@ func keywordsKeys(arguments *arguments.Arguments) {
 	writer.Flush()
 }
 
+func keywordsList(arguments *arguments.Arguments) {
+	var (
+		keywords = keywords.Get()
+	)
+	fmt.Fprintln(writer, "N\t|Name\t|Emoji")
+	keywords.Keys().Sort().Each(func(i int, x interface{}) {
+		key := x.(string)
+		slice := keywords.Fetch(key)
+		fmt.Fprintln(writer, fmt.Sprintf("%v\t|%v\t|%v", i, key, slice.Len()))
+	})
+	writer.Flush()
+}
+
 func keywordsMain(arguments *arguments.Arguments) {
 	switch strings.ToUpper(arguments.Get(0)) {
 	case B, BUILD:
 		build(KEYWORDS, keywords.Make)
 	case K, KEYS:
 		keywordsKeys(arguments.Next())
+	case L, LIST:
+		keywordsList(arguments.Next())
 	}
 }
